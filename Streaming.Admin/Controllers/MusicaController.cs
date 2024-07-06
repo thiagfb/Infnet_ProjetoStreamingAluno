95252efa60000633484979a2c8a078c93a36657a
Thiago Barbosa <thiagfb@hotmail.com>
2024-07-06 15:12:54 -0300

    Criação em MVC da música.

---
+++ Streaming.Admin/Controllers/MusicaController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Streaming.Application.Streaming;
+using Streaming.Application.Streaming.Dto;
+using Streaming.Domain.Streaming.Aggregates;
+
+namespace Streaming.Admin.Controllers
+{
+    public class MusicaController : Controller
+    {
+        private MusicaService musicaService;
+
+        public MusicaController(MusicaService musicaService)
+        {
+            this.musicaService = musicaService;
+        }
+
+        public IActionResult Index()
+        {
+            var result = this.musicaService.GetAll();
+
+            return View(result);
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Salvar(MusicaDto dto)
+        {
+            //if (ModelState.IsValid == false)
+            //    return View("Criar");
+
+            this.musicaService.Criar(dto);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Streaming.Application/Streaming/Dto/MusicaDto.cs
+++ Streaming.Application/Streaming/Dto/MusicaDto.cs
@@ -1,4 +1,5 @@
 ﻿using Streaming.Domain.Streaming.Aggregates;
+using System.ComponentModel.DataAnnotations;
 
 namespace Streaming.Application.Streaming.Dto
 {
@@ -6,14 +7,26 @@ namespace Streaming.Application.Streaming.Dto
     {
         public Guid Id { get; set; }
 
+
+        [Required(ErrorMessage = "Título é obrigatório")] 
         public String Titulo { get; set; }
 
+        [Required(ErrorMessage = "Gênero obrigatório")]
         public Guid GeneroId { get; set; }
 
+        [Required(ErrorMessage = "Compositor é obrigatório")]
         public Guid Compositorid { get; set; }
 
+        [Required(ErrorMessage = "Letra é obrigatória")]
         public String Letra { get; set; }
 
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "Data é obrigatória")]
         public DateTime? DataComposicao { get; set; }
+
+        public Genero Genero { get; set; } = new Genero();
+
+        public Compositor Compositor { get; set; } = new Compositor();
     }
 }
