0a54dd2b3a027bcaaf054477cc187b408d7ef7e0
Thiago Barbosa <thiagfb@hotmail.com>
2024-07-06 13:58:04 -0300

    Estrutura em MVC do cadastro de artista.

---
+++ Streaming.Admin/Controllers/ArtistaController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Streaming.Application.Streaming;
+using Streaming.Application.Streaming.Dto;
+
+namespace Streaming.Admin.Controllers
+{
+    public class ArtistaController : Controller
+    {
+        private ArtistaService artistaService;
+
+        public ArtistaController(ArtistaService artistaService)
+        {
+            this.artistaService = artistaService;
+        }
+
+        public IActionResult Index()
+        {
+            var result = this.artistaService.GetAll();
+
+            return View(result);
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Salvar(ArtistaDto dto)
+        {
+            if (ModelState.IsValid == false)
+                return View("Criar");
+
+            this.artistaService.Criar(dto);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Streaming.Admin/Program.cs
+++ Streaming.Admin/Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using Streaming.Application.Admin;
 using Streaming.Application.Admin.Profile;
+using Streaming.Application.Conta;
+using Streaming.Application.Streaming;
+using Streaming.Application.Streaming.Profile;
+using Streaming.Application.Transacao;
 using Streaming.Repository;
 using Streaming.Repository.Repository;
 
@@ -14,10 +18,48 @@ builder.Services.AddDbContext<StreamingAdminContext>(c =>
      .UseSqlServer(builder.Configuration.GetConnectionString("StreamingConnectionAdmin"));
 });
 
+builder.Services.AddDbContext<StreamingContext>(c =>
+{
+    c.UseLazyLoadingProxies()
+     .UseSqlServer(builder.Configuration.GetConnectionString("StreamingConnection"));
+});
+
 builder.Services.AddAutoMapper(typeof(UsuarioAdminProfile).Assembly);
+builder.Services.AddAutoMapper(typeof(ArtistaProfile).Assembly);
 
 builder.Services.AddScoped<UsuarioAdminRepository>();
 builder.Services.AddScoped<UsuarioAdminService>();
+
+//Repositories
+builder.Services.AddScoped<AlbumRepository>();
+builder.Services.AddScoped<ArtistaRepository>();
+builder.Services.AddScoped<AssinaturaRepository>();
+builder.Services.AddScoped<BandeiraRepository>();
+builder.Services.AddScoped<CartaoRepository>();
+builder.Services.AddScoped<CompositorRepository>();
+builder.Services.AddScoped<FaixaRepository>();
+builder.Services.AddScoped<GeneroRepository>();
+builder.Services.AddScoped<MusicaRepository>();
+builder.Services.AddScoped<PlanoRepository>();
+builder.Services.AddScoped<PlayListMusicaRepository>();
+builder.Services.AddScoped<TransacaoRepository>();
+builder.Services.AddScoped<UsuarioRepository>();
+builder.Services.AddScoped<MusicaPlayListRepository>();
+
+//Services
+builder.Services.AddScoped<AlbumService>();
+builder.Services.AddScoped<ArtistaService>();
+builder.Services.AddScoped<CompositorService>();
+builder.Services.AddScoped<FaixaService>();
+builder.Services.AddScoped<GeneroService>();
+builder.Services.AddScoped<MusicaService>();
+builder.Services.AddScoped<PlanoService>();
+builder.Services.AddScoped<PlayListMusicaService>();
+builder.Services.AddScoped<BandeiraService>();
+builder.Services.AddScoped<CartaoService>();
+builder.Services.AddScoped<TransacaoService>();
+builder.Services.AddScoped<UsuarioService>();
+builder.Services.AddScoped<MusicaPlayListService>();
 
 var app = builder.Build();
 
