9b0e5c1b538ee4e71a3515b8b5d8a27fbaf5c920
Thiago Barbosa <thiagfb@hotmail.com>
2024-09-22 21:39:36 -0300

    Removendo o swagger em tempo de desenvolvimento para rodar no Azure.

--- Streaming.Api/Program.cs
+++ Streaming.Api/Program.cs
@@ -90,11 +90,11 @@ var app = builder.Build();
 app.UseCors();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
